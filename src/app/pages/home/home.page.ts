90ea15a696d8cbd1f71c3592f7581b94ce8c0dcd
DiCabrera <diegoc1739@gmail.com>
2024-06-08 21:01:14 -0400

    Implementación geolocation API
    Dentro del botón SOS hay una opción para compartir la ubicación

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { Share, ShareOptions } from '@capacitor/share';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,7 @@ export class HomePage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
   isModalOpen = false;
@@ -18,4 +21,28 @@ export class HomePage implements OnInit {
     this.isModalOpen = isOpen;
   }
 
+  async location(){
+    const coordenada = await Geolocation.getCurrentPosition();
+    var lat = coordenada.coords.latitude;
+    var lon = coordenada.coords.longitude;
+   
+    var locationUrl = "http://maps.google.com/maps?q="+lat+","+lon; 
+    const titleShare = "SOS";
+    const textShare = "SOS ⚠ ¡Requiero ayuda urgente! Me encuento aquí: "
+
+    var shareOptions:ShareOptions = {
+      title:titleShare,
+      text:textShare,
+      url:locationUrl
+    }
+    
+    await Share.share(shareOptions);
+
+    console.log("COORDENADAS: ", lat, lon);
+    console.log("http://maps.google.com/maps?q="+lat+","+lon);
+    console.log(shareOptions);
+    
+  }
+
+  
 }
